628a613fc05348d44e796eaa674701562ec7a20e
Yingzhongjiang <yjiang3@babson.edu>
2019-04-15 11:44:17 -0400

    added paste function via python

--- part3.py
+++ part3.py
@@ -2,29 +2,42 @@ import random
 from random import randint
 from PIL import Image
 
-#randomly generates x forground elements to overlay
+#randomly generates x forground elements
 """
 1. random selection from environment list
 2. randomly places random selection onto screen
+# """
+ONE=["octo.jpg", "beachball.jpg", "eagle.png"] #BEACH
+TWO=[] #FOREST
+THREE=[] #MOUNTAIN
+FOUR=[] #DESERT
+
+num_to_select=randint(0,9)
+list_of_random_images=random.sample(ONE,num_to_select)
+
+# #-----------------------------------------
+
+#overlay image onto final screen
 """
-# ONE=['KRAKEN','CRAB','BEACHBALL','PALM TREE','SEASHELL','SEAGULL','UMBRELLA','GIANT SQUID', "SUNTAN LOTION", 'MONKEY'] #BEACH
-# TWO=[] #FOREST
-# THREE=[] #MOUNTAIN
-# FOUR=[] #DESERT
+puts image onto other image (eg middleground (sand), and foreground (octopus))
+"""
+# #------the manual version------
+# sand=Image.open("sand.jpg")
+# beachball=Image.open("beachball.jpg")
+# octo=Image.open("octo.jpg")
 #
-# num_to_select=randint(0,9)
-# list_of_random_images=random.sample(ONE,num_to_select)
+# a=(100,100, 650,650)
+# m=(10,10,910,530)
 #
-# print(list_of_random_images)
-# -----------------------------------------
+# sand.paste(beachball, a) #puts beachball on sand, with specifi coords
+# sand.paste(octo,m)
+# #sand.show()
 
+# -----the automatic version------
+sand=Image.open("sand.jpg")
 
+for i in list_of_random_images:
+    element=Image.open(i)
+    sand.paste(element)
 
-background = Image.open(r"/home/yjiang/softdes_final_project/eagle.png")
-overlay = Image.open(r"/home/yjiang/softdes_final_project/octo.jpg")
-
-background = background.convert("RGBA")
-overlay = overlay.convert("RGBA")
-
-new_img = Image.blend(background, overlay, 0.5)
-new_img.save("new.png","PNG")
+sand.show()
