a6997a48489109882329ff5dc5069994bffab74a
Shirin Kuppusamy <skuppusamy@olin.edu>
2019-04-19 12:28:39 -0400

    Background Image Generation

--- Background Image/background.py
+++ Background Image/background.py
@@ -7,9 +7,9 @@ def clear_background():
     Generates a clear background image so that when overlayed on another page,
     the background of that page shows through.
     """
-    background = Image.new("RGB", (255, 255))
-    background.putalpha(0)
-    return background
+    clearbackground = Image.new("RGB", (255, 255))
+    clearbackground.putalpha(0)
+    return clearbackground
 
 def background(s,x_size=255, y_size=255):
     """Generates background based on time of day and saves as an image file.
@@ -18,8 +18,8 @@ def background(s,x_size=255, y_size=255):
         s: string that tells what time of day it is
         x_size, y_size: optional args to set image dimensions (default: 255)
     """
-    im = Image.new("RGB", (x_size, y_size))
-    pixels = im.load()
+    background = Image.new("RGB", (x_size, y_size))
+    pixels = background.load()
     for i in range(x_size):
         for j in range(y_size):
             x = i
@@ -45,15 +45,11 @@ def background(s,x_size=255, y_size=255):
                 b = 0
                 c = y
             pixels[i, j] = (
-                # color_map(evaluate_random_function(red_function, x, y)),
-                # #color_map(0),
-                # color_map(evaluate_random_function(green_function, x, y)),
-                # color_map(evaluate_random_function(blue_function, x, y))
                 a,
                 b,
                 c
             )
-    return im
+    return background
 
 def middleground(s):
     """
@@ -72,23 +68,10 @@ def middleground(s):
 
 def combine_back_middle(im1, im2):
     """
-    This combines the the middleground and foreground images.
+    This combines the the middleground and background images.
     """
-    im = Image.blend(im1,im2,0.5)
-    return im
-
-
-# def generate_art(filename,s,x_size=255, y_size=255):
-#     """Generates background based on time of day and saves as an image file.
-#     Args:
-#         filename: string filename for image (should be .png)
-#         s: string that tells what time of day it is
-#         x_size, y_size: optional args to set image dimensions (default: 255)
-#     """
-#     # Create image and loop over all pixels
-#     im = Image.new("RGB", (x_size, y_size))
-#     im.putalpha(0)
-#     im3 = Image.new("RGB",(x_size, y_size)
+    im1.paste(im2,(0,100))
+    return im1
 
 if __name__ == '__main__':
     import doctest
