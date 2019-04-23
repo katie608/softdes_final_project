269b8b47d4208b780bcd3728db80a68fc5c1bcdb
katie608 <katief650@gmail.com>
2019-04-23 18:33:27 -0400

    Delete old website files, move new ones up a directory

--- flask/README.md
+++
@@ -1,7 +0,0 @@
-# Toolbox-Flask
-
-Web Apps Project Toolbox starter code
-
-Full instructions on [the course website](https://sd19spring.github.io/toolboxes/web-apps).
-
-This toolbox exercise was developed by [Patrick Huston](https://github.com/phuston).
--- getinfo.py
+++ getinfo.py
@@ -1,7 +1,6 @@
-"""Attempt 1 at integration, and getting variables from the website
-into the python
+"""Python code using flask to get variables from a website
+Katie Foster
 """
-import random
 # imports flask class
 from flask import Flask, render_template, request
 import requests
@@ -9,28 +8,21 @@ import requests
 # first argument is the name of the application’s module or package
 app = Flask(__name__)
 
+# tells which url should trigger our function. In this case, it is / because
+# it is the homepage of the website.
 
-# renders html hopefully
 @app.route('/', methods = ["GET","POST"])
-def index():
+def get_input():
     return render_template("index.html")
 
-
-
-# will render the output html with variables from first html
 @app.route('/output', methods = ["GET","POST"])
 def display_output():
     scene = request.form.get('scene')
-    time = request.form.get('time')
+    date = request.form.get('date')
+    dims = request.form.get("dims")
     if scene == "":
-        scene = random.randint(1,4)
-    # elif time == "":
-    #     return render_template('error.html')
-    print("browser time: ", request.args.get("time"))
-    time = request.args.get("time")
-    print("server time : ", time.strftime('%A %B, %d %Y %H:%M:%S'));
-    return render_template('generated.html', scene=scene, time=time)
-
+        return render_template('error.html')
+    return render_template('output.html', scene=scene, date=date, dims=dims)
 
 # runs local server with our application
 # if __name__ == '__main__' makes sure the server only runs if the script is
