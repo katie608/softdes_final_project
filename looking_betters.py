83cefe4555ece16f55fe06586c7e10671d74747e
Yingzhongjiang <yjiang3@babson.edu>
2019-04-23 12:37:11 -0400

    code for creating progressive jpegs

--- looking_betters.py
+++ looking_betters.py
@@ -1,12 +1,20 @@
 import random
 from random import randint
 import math
-from PIL import Image
+from PIL import Image, ImageFile
 #modified material in background.py
 
-"""resize photos"""
-image=Image.open('octo.png')
-image.show()
-image.thumbnail((400,400))
-image.save("smallerocto.jpg")
-image.show()
+# """resize photos"""
+# image=Image.open('octo.png')
+# image.show()
+# image.thumbnail((400,400))
+# image.save("smallerocto.jpg")
+# image.show()
+
+
+""""save image as progressive image"""
+ImageFile.MAXBLOCK = 2**20
+
+img = Image.open("octo.png")
+img.save("out.jpg", "JPEG", quality=80, optimize=True, progressive=True)
+img.show()
