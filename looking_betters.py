8b1baceff4ea8252f10264027786fc9a82295638
Yingzhongjiang <yjiang3@babson.edu>
2019-04-26 22:47:27 -0400

    progressive jpegs, removing whitespace from foreground images, updates on sillhoutte creator

--- looking_betters.py
+++ looking_betters.py
@@ -19,23 +19,21 @@ from PIL import Image, ImageFile
 # img.save("out.jpg", "JPEG", quality=80, optimize=True, progressive=True)
 # img.show()
 
-#-----------the new code to implement--------------
+#---------------the new code to implement--------------
 
 # """color overlay based on time of day, the final layer to put onto image generation
 # current time modes:
-#     dawn
-#     morning
-#     noon
-#     evening
-#     night
+#     dawn (pinkish)
+#     morning and noon (no color overlay)
+#     evening (sunset colors)
+#     night (nighttime dark blue colors)
 # """
+##note for when we merge code: we will need to get a folder for color overlay jpegs
+##to make sure we have everything neat
+#
 # def timecoloroverlay(): #then put daytime image onto the final image
 #     if time == dawn: #question: is this based on the time recognition in the JavaScript code? Needs integration here
 #         img = Image.open("dawnpink.png")
-#         img.putalpha(10)
-#         img.save()
-#     elif time == morning:
-#         img=Image.open("lightblue.png")
 #         img.putalpha(10)
 #         img.save()
 #     elif time ==noon: #a pure transparent overlay
@@ -65,36 +63,40 @@ from PIL import Image, ImageFile
 #         if pixdata[x, y] == (255, 255, 255, 255):
 #             pixdata[x, y] = (255, 255, 255, 0)
 # img.show()
-# img.save("img2.png", "PNG")
+## img.save("img2.png", "PNG")
+
+
+# """save files as progressive jpegs"""
+# #this will be at the very end of the code to
+# #save the final picture as progressive jpeg
+# def final_save_image_settings():
+#     img=Image.open("octo.png") #replace w/ final image from master Python code
+#     img.save("softdes final project", format="JPEG", quality=95, progressive=True)
+#     img.show()
 
 
 
 
-"""sillhoutes of images""" ###STOPPED HERE
-im=Image.open("octo.png")
-im=im.convert("RGBA")
+"""sillhoutes of images based on time:
+    dawn (sunrise, so light gray sillhoutes)
+    noon and daytime (no or transparent sillhouttes)
+    evening (sunset, so dark gray sillhoutes)
+    night (almost complete black/blue sillhoutte)
+"""
+def sillhoutte_maker():
 
 
-#---------version one
+
+
+#this currently makes things transparent
+img = Image.open('octo.png')
+img = img.convert("RGBA")
 pixdata = img.load()
 
-# Clean the background noise, if color != white, then set to black.
-
-for y in xrange(img.size[1]):
-    for x in xrange(img.size[0]):
+width, height = img.size
+for y in xrange(height):
+    for x in xrange(width):
         if pixdata[x, y] == (255, 255, 255, 255):
-            pixdata[x, y] = (0, 0, 0, 255)
-
+            pixdata[x, y] = (255, 255, 255, 0)
 img.show()
-
-
-#---------version two
-# data = np.array(im)   # "data" is a height x width x 4 numpy array
-# red, green, blue, alpha = data.T # Temporarily unpack the bands for readability
-#
-# # Replace white with red... (leaves alpha values alone...)
-# white_areas = (red == 255) & (blue == 255) & (green == 255)
-# data[..., :-1][white_areas.T] = (255, 0, 0) # Transpose back needed
-#
-# im2 = Image.fromarray(data)
-# im2.show()
+img.save("img2.png", "PNG")
